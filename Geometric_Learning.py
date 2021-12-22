b52f55e3b57f0b1bc443376b3eae17a68a77a43d
SuperrWu <ubuntu@localhost.localdomain>
2021-12-22 18:44:36 +0800

    Convert Networkx to DLG.Data.data

--- Geometric_Learning.py
+++ Geometric_Learning.py
@@ -6,15 +6,40 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from CreateGraph import BrainNetwork
 import dgl
+from dgl.data import DGLDataset
+from tqdm import tqdm
+import torch
 
+class SyntheticDataset(DGLDataset):
+    def __init__(self):
+        super().__init__(name='synthetic')
+
+    def process(self):
+        self.graphs = []
+        self.labels = []
+        EEGhandler = EEGHandler()
+        path = "eegtrialsdata.mat"
+        x, y = EEGhandler.load_eeg(path)
+        for i in tqdm(x):
+            functional_connectivity = EEGhandler.compute_functional_connectivity(i)
+            adjacency = EEGhandler.thresholding(functional_connectivity, 0.6, ignore_negative = False)
+            brainnet = BrainNetwork(adjacency)
+            g = dgl.from_networkx(brainnet.Graph)
+            self.graphs.append(g)
+            self.labels.append(0)
+
+        # Convert the label list to tensor for saving.
+        self.labels = torch.LongTensor(self.labels)
+
+    def __getitem__(self, i):
+        return self.graphs[i], self.labels[i]
+
+    def __len__(self):
+        return len(self.graphs)
 
 
 if __name__ == "__main__":
-    EEGhandler = EEGHandler()
-    path = "eegtrialsdata.mat"
-    x, y = EEGhandler.load_eeg(path)
-    data = x[0]
-    functional_connectivity = EEGhandler.compute_functional_connectivity(data)
-    adjacency = EEGhandler.thresholding(functional_connectivity, 0.6, ignore_negative = False)
-    brainnet = BrainNetwork(adjacency)
-    dgl.from_networkx(brainnet.Graph)
+    dataset = SyntheticDataset()
+    graph, label = dataset[0]
+    print(graph, label)
+
